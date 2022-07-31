cca90f13c2c4d0f244d605c7cbd6da77dfcda5b9
dm <dmitrij.demidovich@gmail.com>
2022-07-31 13:18:04 +0300

    add debug cli mode

--- php/util/debug.php
+++ php/util/debug.php
@@ -2,13 +2,28 @@
 
 function dd(...$args)
 {
-    echo "<pre>";
+    if (php_sapi_name() === "cli") {
+        $startTag = $finalTag = "\n";
+    } else {
+        $startTag = $finalTag = "<pre>";
+    }
+
+    $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+    $filename  = "noname";
+    if (! empty($backtrace[0]["file"])) {
+        $filename = $backtrace[0]["file"];
+        $filename = str_replace(getcwd() . "/", "", $filename);
+    }
+
+    echo $startTag;
+    echo $filename;
+    echo "\n\n";
 
     foreach ($args as $arg) {
         print_r($arg);
         echo "\n";
     }
 
-    echo "</pre>";
+    echo $finalTag;
     exit;
 }
